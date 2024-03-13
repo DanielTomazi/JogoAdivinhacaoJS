06818b4cc1e598042d9604f1b95f2ef8cdad84ab
Daniel Tomazi de Oliveira <125170893+DanielTomazi@users.noreply.github.com>
2024-03-13 15:00:00 -0300

    Inserção do código
    Inserção do código e projeto final.

---
+++ Jogo Adivinhação JS/app.js
@@ -0,0 +1,30 @@
+alert("Boas vindas ao game do número secreto");
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute 
+let numeroTentativas = 1;
+
+
+while (chute != numeroSecreto){
+    chute = prompt("Escolha um número de 1 a " + numeroMaximo);
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+         if (chute > numeroSecreto){
+        alert("O número secreto é menor que " + chute);
+    } else {
+        alert("O número secreto é maior que " + chute);
+        }
+        numeroTentativas++;
+    } 
+}
+
+let palavraTentativa = numeroTentativas > 1 ? "tentativas" : "tentativa";
+alert("Muito bem, você descobriu o número secreto " + (numeroSecreto) + " com " + numeroTentativas +  palavraTentativa);
+
+//if (numeroTentativas > 1) {
+//    alert("Muito bem, você descobriu o número secreto " + (numeroSecreto) + " com " + numeroTentativas + " tentativas." );
+//} else {
+//    alert("Muito bem, você descobriu o número secreto " + (numeroSecreto) + " com " + numeroTentativas + " tentativa." );
+//}
