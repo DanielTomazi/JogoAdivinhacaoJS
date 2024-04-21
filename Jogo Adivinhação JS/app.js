e45a9b6d62e8aa2db1d1bb107126f6f7dbbd47a6
Daniel Tomazi de Oliveira <125170893+DanielTomazi@users.noreply.github.com>
2024-04-20 22:38:40 -0300

    Update app.js

--- Jogo Adivinhação JS/app.js
+++ Jogo Adivinhação JS/app.js
@@ -1,5 +1,5 @@
 alert("Boas vindas ao game do número secreto");
-let numeroMaximo = 1000;
+let numeroMaximo = 10;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute 
